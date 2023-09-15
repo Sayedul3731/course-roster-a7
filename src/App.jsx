fbb5901cb086c184d712e15ee449a40309517d85
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-09-14 19:13:00 -0700

    added project header style

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-5xl font-semibold text-center'>Course Registration</h1>
+      <h1 className='text-3xl font-bold text-center mt-12 mb-8'>Course Registration</h1>
     </>
   )
 }
